c2c36dceadc3767aadfd75897ab263ec1e1173d5
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-09-01 14:27:10 +0300

    .

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import { Box, Typography } from "@mui/material";
 
-const HorizontalScrollbar = () => {
-  return (
-    <div>HorizontalScrollbar</div>
-  )
-}
+import ExerciseCard from "./ExerciseCard";
+import BodyPart from "./BodyPart";
 
-export default HorizontalScrollbar
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
+  <ScrollMenu>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        overflowX: "auto",
+        width: "100vw", // Set the width to 100% of viewport width
+      }}
+    >
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          m="0 40px"
+        >
+          {bodyParts ? (
+            <BodyPart
+              item={item}
+              setBodyPart={setBodyPart}
+              bodyPart={bodyPart}
+            />
+          ) : (
+            <ExerciseCard exercise={item} />
+          )}
+        </Box>
+      ))}
+    </Box>
+  </ScrollMenu>
+);
+
+export default HorizontalScrollbar;
