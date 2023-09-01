48a95e0c589bbc32f4b455c253a5eb8bb2c9a38e
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-09-01 08:00:36 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import {Box} from '@mui/material';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box width='400px'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/exercise:id' element={<ExerciseDetail} />
+      </Routes>
+    </Box>
+
   );
 }
 
