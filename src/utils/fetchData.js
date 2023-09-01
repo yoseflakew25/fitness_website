a9360445aaac9954fe49843b8be5239b815ed477
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-09-01 14:52:56 +0300

    .

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,10 +7,14 @@ export const exerciseOptions = {
 };
 
 export const youtubeOptions = {
-  method: 'GET',
+  method: "GET",
+  url: "https://youtube-search-and-download.p.rapidapi.com/channel/about",
+  params: {
+    id: "UCE_M8A5yxnLfW0KghEeajjw",
+  },
   headers: {
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    "X-RapidAPI-Key": "43be71910fmsh5c4c0ce7aab8a99p11ee2cjsne4a147f158de",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
 
